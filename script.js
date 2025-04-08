41d71fccbe474c48e69bb15198ee1ce510a527ed
Alexander Vilchez <tivilexwork@gmail.com>
2025-04-08 15:17:52 -0500

    Fusion de ramas correcta

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ var letras = [
     { letra: 'F', palabra: 'Foca', imagen: 'Pictures/Foca.png' },
     { letra: 'G', palabra: 'Gato', imagen: 'Pictures/Gato.png' },
     { letra: 'H', palabra: 'Hipopotamo', imagen: 'Pictures/Hipo.png' },
+    { letra: 'I', palabra: 'Iguana', imagen: './pictures/Iguana.jpg' },
+    { letra: 'J', palabra: 'Jarra', imagen: './pictures/Jarra.jpg' },
+    { letra: 'K', palabra: 'Koala', imagen: './pictures/Koala.jpg' },
+    { letra: 'L', palabra: 'Luna', imagen: './pictures/Luna.png' },
+    { letra: 'M', palabra: 'Manzana', imagen: './pictures/Manzana.png' },
+    { letra: 'N', palabra: 'Naranja', imagen: './pictures/Naranja.png' },
 ];
 
 var posicion = 0;
@@ -16,6 +22,12 @@ function mostrarLetra() {
     document.getElementById("palabra").innerText = letras[posicion].palabra;
     document.getElementById("imagen").src = letras[posicion].imagen;
     document.getElementById("imagen").alt = letras[posicion].palabra;
+}
+
+function cambiarColor() {
+    const colores = ["red", "blue", "green", "pink"];
+    const colorRamdom = colores[Math.floor(Math.random() * colores.length)];
+    document.getElementById("card").style.backgroundColor = colorRamdom;
 }
 
 function siguiente() {
@@ -46,54 +58,11 @@ function aplicarColoresAleatorios(elemento) {
     elemento.style.background = `linear-gradient(45deg, ${colores[0]}, ${colores[1]}, ${colores[2]})`;
 }
 
-
 document.querySelectorAll('h1').forEach(h1 => {
     h1.addEventListener('click', () => aplicarColoresAleatorios(h1));
 });
 
-
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => aplicarColoresAleatorios(button));
 });
-var letras = [
-    { letra: 'I', palabra: 'Iguana', imagen: './pictures/Iguana.jpg' },
-    { letra: 'J', palabra: 'Jarra', imagen: './pictures/Jarra.jpg' },
-    { letra: 'K', palabra: 'Koala', imagen: './pictures/Koala.jpg' },
-    { letra: 'L', palabra: 'Luna', imagen: './pictures/Luna.png' },
-    { letra: 'M', palabra: 'Manzana', imagen: './pictures/Manzana.png' },
-    { letra: 'N', palabra: 'Naranja', imagen: './pictures/Naranja.png' },
-  ];
-  
-  var posicion = 0;
-  
-  function mostrarLetra() {
-    document.getElementById("letra").innerText = letras[posicion].letra;
-    document.getElementById("palabra").innerText = letras[posicion].palabra;
-    document.getElementById("imagen").src = letras[posicion].imagen;
-    document.getElementById("imagen").alt = letras[posicion].palabra;
-  }
 
-  function cambiarColor() {
-    const colores = ["red","blue", "green","pink"]
-    const colorRamdom = colores[Math.floor(Math.random()* colores.length)]
-    document.getElementById("card").style.backgroundColor = colorRamdom
-  }
-  
-  function siguiente() {
-    posicion = posicion + 1;
-    if (posicion >= letras.length) {
-      posicion = 0;
-    }
-    mostrarLetra();
-  }
-  
-  function anterior() {
-    posicion = posicion - 1;
-    if (posicion < 0) {
-      posicion = letras.length - 1;
-    }
-    mostrarLetra();
-  }
-  
-  mostrarLetra();
-  
