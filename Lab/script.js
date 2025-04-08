baee76cfa03b3a05068f189e3cbcfbbdc7ca85fe
Angelina812 <gabycultid@gmail.com>
2025-04-08 14:44:07 -0500

    Agregando rama tz de abecedario

---
+++ Lab/script.js
@@ -0,0 +1,37 @@
+var letras = [
+    { letra: 'T', palabra: 'tomate', imagen: 'Pictures/tomate.png' },
+    { letra: 'U', palabra: 'uvas', imagen: 'Pictures/uvas.jpg' },
+    { letra: 'V', palabra: 'vaca', imagen: 'Pictures/vaca.jpg' },
+    { letra: 'W', palabra: 'wifi', imagen: 'Pictures/wifi.jpg' },
+    { letra: 'X', palabra: 'xilofono', imagen: 'Pictures/xilofono.jpg' },
+    { letra: 'Y', palabra: 'yuca', imagen: 'Pictures/yuca.jpg' },
+    { letra: 'Z', palabra: 'zapato', imagen: 'Pictures/zapato.jpg' },
+  ];
+  
+  var posicion = 0;
+  
+  function mostrarLetra() {
+    document.getElementById("letra").innerText = letras[posicion].letra;
+    document.getElementById("palabra").innerText = letras[posicion].palabra;
+    document.getElementById("imagen").src = letras[posicion].imagen;
+    document.getElementById("imagen").alt = letras[posicion].palabra;
+  }
+  
+  function siguiente() {
+    posicion++;
+    if (posicion >= letras.length) {
+      posicion = 0;
+    }
+    mostrarLetra();
+  }
+  
+  function anterior() {
+    posicion--;
+    if (posicion < 0) {
+      posicion = letras.length - 1;
+    }
+    mostrarLetra();
+  }
+  
+  mostrarLetra();
+  
